20bb28527ff4c6d97156556b9ef3e7f5b9ece891
Pmyl <julo134@gmail.com>
2022-02-14 15:18:20 +0000

    add deps in template file

--- select/webpack-require-context.js
+++ select/webpack-require-context.js
@@ -1,4 +1,6 @@
 /* globals require */
+import 'core-js';
+import 'zone.js';
 import 'CUSTOM_BASE_TEST_FILE_PATH';
 
 var appContext = require.context('KARMA_FAST_FILE_DIR', true, /KARMA_FAST_FILE_NAME$/);
